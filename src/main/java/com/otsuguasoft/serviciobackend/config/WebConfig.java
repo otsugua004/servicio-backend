6751278163182e9ff2d848c64550ef0336c13f1a
otsugua04 <augusto.gonzalez@fisagrp.com>
2025-11-07 01:51:41 -0500

    permitir cors

--- src/main/java/com/otsuguasoft/serviciobackend/config/WebConfig.java
+++ src/main/java/com/otsuguasoft/serviciobackend/config/WebConfig.java
@@ -1,4 +1,18 @@
 package com.otsuguasoft.serviciobackend.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todas las rutas
+                .allowedOrigins("*") // Permite cualquier origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 }
