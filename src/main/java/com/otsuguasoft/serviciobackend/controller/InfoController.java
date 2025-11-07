1e20da4bdc4337f2017f96c73746e1a5387cf8f5
otsugua04 <augusto.gonzalez@fisagrp.com>
2025-11-07 01:15:38 -0500

    llamar a servicio bdd

--- src/main/java/com/otsuguasoft/serviciobackend/controller/InfoController.java
+++ src/main/java/com/otsuguasoft/serviciobackend/controller/InfoController.java
@@ -1,6 +1,9 @@
 package com.otsuguasoft.serviciobackend.controller;
 
 import com.otsuguasoft.serviciobackend.dto.InfoResponse;
+import com.otsuguasoft.serviciobackend.dto.InformacionDTO;
+import com.otsuguasoft.serviciobackend.service.ConsultaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,9 +12,15 @@ import java.util.Map;
 @RequestMapping("/api/info")
 public class InfoController {
 
+    private final ConsultaService consultaService;
+
+    public InfoController(ConsultaService consultaService) {
+        this.consultaService = consultaService;
+    }
+
     @GetMapping("/{id}")
-    public InfoResponse getInfoById(@PathVariable("id") String id) {
-        return new InfoResponse("Información recibida para el ID: " + id);
+    public InformacionDTO getInfoById(@PathVariable("id") Long id) {
+        return consultaService.consultarPorId(id);
     }
 
 }
